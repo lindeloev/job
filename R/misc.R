503072ca97cb37b5061fbb0429538867ee14450c
Jonas Kristoffer Lindeløv <jokl@kamstrup.com>
2021-04-27 16:14:36 +0200

    RStudio Addins #6

--- R/misc.R
+++ R/misc.R
@@ -7,3 +7,32 @@
 print.jobcode = function(x, ...) {
   cat(x)
 }
+
+
+jobaddin_run = function(pattern) {
+  j__ <<- list()
+  j__$selection = rstudioapi::selectionGet()$value
+  j__$title = gsub("\n", "; ", substr(j__$selection, 1, 80))
+  j__$call = gsub("JOBCODE", j__$selection, gsub("JOBTITLE", j__$title, pattern))
+  eval(parse(text = j__$call), envir = globalenv())
+  rm(j__, envir = globalenv())
+}
+
+jobaddin_selection = function() {
+  jobaddin_run("job::job({JOBCODE}, title = 'JOBTITLE')")
+}
+
+jobaddin_selection_empty = function() {
+  jobaddin_run("job::job({JOBCODE}, title = 'JOBTITLE', import = NULL, packages = NULL, opts = NULL)")
+}
+
+
+
+if (FALSE) {
+  my_data = mtcars
+  fit = lm(mpg ~ cyl + hp, my_data)
+  predict(fit)
+  rm(list = ls())
+
+  fit = brms::brm(mpg ~ cyl, mtcars)
+}
