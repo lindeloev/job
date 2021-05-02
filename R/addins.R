05a7ee95575dcdd2c0a1dbe7596be579694b92da
Jonas Kristoffer Lindeløv <jokl@kamstrup.com>
2021-05-03 00:48:12 +0200

    Addins support apostrophes now (#16)

--- R/addins.R
+++ R/addins.R
@@ -1,9 +1,13 @@
 jobaddin_run = function(pattern) {
   j__ <<- list()  # Don't know why it has to be in global...
   j__$selection = rstudioapi::selectionGet()$value
-  j__$title = gsub("\n", "; ", substr(j__$selection, 1, 80))
-  j__$call = gsub("JOBCODE", j__$selection, gsub("JOBTITLE", j__$title, pattern))
-  eval(parse(text = j__$call), envir = globalenv())
+  if (length(j__$selection) == 0 || j__$selection == "") {
+    rstudioapi::showDialog("Error", "Error: No code was selected.")
+  } else {
+    j__$title = gsub("'", "\\\\'", fixed = TRUE, x = gsub("\n", "; ", substr(j__$selection, 1, 80)))
+    j__$call = gsub("JOBCODE", j__$selection, gsub("JOBTITLE", j__$title, pattern))
+    eval(parse(text = j__$call), envir = globalenv())
+  }
   rm(j__, envir = globalenv())
 }
 
